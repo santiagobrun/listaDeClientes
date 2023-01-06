37da4c82b4a02111bf49210e4b9ab5ee0d4a898d
Santiago <Santiago@192.168.0.11>
2023-01-05 22:47:00 -0300

    Aplicacion para guardar informacion de personas y mostrarlo en una lista.

---
+++ src/main/java/com/mycompany/sistemadegestion/SistemaDeGestion.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemadegestion;
+
+/**
+ *
+ * @author Santiago Brunet
+ */
+public class SistemaDeGestion {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+        
+    }
+}
